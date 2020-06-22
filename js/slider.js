a1c9d986c4b7f69e3f23a1f01d207d6aee10f18a
DanLisow <56221679+DanLisow@users.noreply.github.com>
2020-06-22 05:49:17 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,3 @@
-  // $('.owl-carousel').owlCarousel({
-  //   responsive: {
-  //     480 : {
-  //       items: 3
-  //     },
-  //     1024 : {
-  //       items: 5
-  //     }
-  //   }
-  // });
-
   $('.local-slider__block').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
@@ -18,9 +7,16 @@
     nextArrow: '.slick-next-local',
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+          breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
                 slidesToScroll: 1
             }
         }
@@ -38,25 +34,76 @@
     cssEase: 'linear'
   })
 
-
+  
   $('.types-slider').slick({
     infinite: true,
     dots: true,
     slidesToShow: 4,
     slidesToScroll: 4,
     speed: 800,
-    appendDots: '.types-attributes__dots',
-    dotsClass: '.types-attributes__dots',
     arrows: true,
     appendArrows: '.types-attributes__arrows',
     prevArrow: '.slick-prev-types',
     nextArrow: '.slick-next-types',
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.decision-slider').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    speed: 800,
+    arrows: true,
+    appendArrows: '.decision-attributes__arrows',
+    prevArrow: '.slick-prev-decision',
+    nextArrow: '.slick-next-decision',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
 
-
-  let slide = document.querySelectorAll('.types-attributes__dots li');
-  slide.forEach(element => {
-    element.innerHTML = "";
-    element.classList.add('dots');
-  });
+ 
